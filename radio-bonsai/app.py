05bd61bce439e494c2b62f951e6ca73d6043420c
Alfsn49 <edgar.amanta@espoch.edu.ec>
2025-09-15 15:52:08 -0500

    Remove reload option from socketio.run for improved stability

--- radio-bonsai/app.py
+++ radio-bonsai/app.py
@@ -210,4 +210,4 @@ def comentario():
     return '', 204
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8000, reload=True)
+    socketio.run(app, host='0.0.0.0', port=8000)
